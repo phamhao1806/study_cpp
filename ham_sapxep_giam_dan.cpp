310f1b41abbde20250f2872a2f03ae23dd97bf9f
pham_hao <haovippc@gmail.com>
2022-03-22 22:20:16 +0700

    sap xep giam dan trong mang

---
+++ ham_sapxep_giam_dan.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+void sapxep(int a[100], int n){
+	int tg;
+	for(int i = 0; i < n; i++){
+	  for(int j = i+1; j < n; j++){
+		  if(a[i] < a[j]){
+		      tg = a[i];
+		      a[i] = a[j];
+		      a[j] =tg;
+	 	  } 
+		}      
+	}
+}	
+int main(){
+    int a[100];
+	int n;
+	cout << "nhap gia tri n: ";
+	cin >> n;
+	cout << "nhap phan tu mang " << endl;
+	for(int i = 0; i < n; i++){
+		cin >> a[i];
+	}
+	cout << "day sau khi nhap : "<< endl;
+	for(int i = 0; i < n; i++){
+	    cout <<"a["<< i <<"]= " << a[i] << endl;
+	}
+	sapxep(a, n);
+	for(int i = 0; i < n; i++){
+		cout << a[i] << " " ;
+	}	
+	return 0;		
+}		
+		
+		
