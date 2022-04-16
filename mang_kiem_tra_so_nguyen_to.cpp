86a0112931d23a77c513925ece3b21262c587324
pham_hao <haovippc@gmail.com>
2022-04-16 14:14:48 +0700

    mang kiem tra so nguyen to

---
+++ mang_kiem_tra_so_nguyen_to.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+bool songuyento(int n){
+	if(n < 2){
+		return false;
+	}
+	int count = 0;
+	for(int i = 2; i <= sqrt(n); i++){
+	    if(n % i == 0){
+		    count ++;
+		}
+	}
+	if( count == 0){
+	    return true;
+	} else{
+		return false;
+	}				
+}
+int main(){
+	int n;
+	cout << "nhap n:";
+	cin >> n;
+	if(songuyento(n) == true){
+		cout << "day la so nguyen to" << endl;
+	}else{
+		cout << "day khong phai la so nguyen to" << endl;
+	}	
+	return 0;
+}	
+	
