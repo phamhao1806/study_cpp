8d0842d456fb880b232e2e8c23022ad92bf982f8
pham_hao <haovippc@gmail.com>
2022-04-18 00:01:44 +0700

    ham so chinh phuongv

--- ham_so_chinh_phuong.cpp
+++ ham_so_chinh_phuong.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
 #include <math.h>
- 
 using namespace std;
- 
 bool scp(int n){
     int sqr = sqrt(n);
     return (sqr*sqr == n);
 }
- 
 int main(){
-    
     int n;
     do{
         cout << "Nhap n = ";
@@ -17,7 +13,7 @@ int main(){
     }while(n <= 0);
     int a[n];
     for(int i = 0;i < n;i++) cin >> a[i];
-    cout << "Cac so chinh phuong:" << endl;
+    cout << "Cac so chinh phuong:"<< endl;
     
     for(int i = 0;i < n; i++){
         if(scp(a[i])){
