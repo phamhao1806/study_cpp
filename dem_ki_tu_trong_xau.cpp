a4c75a1f64d9fd5085a68adce6b32935de23ce7f
pham_hao <haovippc@gmail.com>
2022-04-23 22:08:55 +0700

    dem ki tu trong xau

--- dem_ki_tu_trong_xau.cpp
+++ dem_ki_tu_trong_xau.cpp
@@ -9,7 +9,7 @@ int main() {
     int count = 0;
     for (int i = 0; i < s.size(); i++) {
         if (s[i] == c) {
-        	cout << i << endl;
+            cout << i << endl;
             count++; 
         }
     }
