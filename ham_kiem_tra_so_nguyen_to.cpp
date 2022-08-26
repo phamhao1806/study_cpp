80b7d4667e81ddd00c478246a1dcf32db2d6e166
hao <haovippc@gmail.com>
2022-08-26 14:56:37 +0700

    ham kiem tra so nguyen to

---
+++ ham_kiem_tra_so_nguyen_to.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int kiemtra(int n){
+	if(n < 2){
+		cout << "day khong phai so nguyen to";
+	}
+	int dem = 0;
+	for(int i = 2; i <= sqrt(n); i++){
+		if( n % i == 0){
+			dem ++;
+			break;
+		}
+	}
+	if(dem == 0){
+		cout << n << " la so nguyen to" << endl;
+	}else{
+		cout << n << " khong la so nguyen to" << endl;
+	}
+	return n;
+}
+
+int main(){
+	int n;
+	cout << "nhap n: ";
+	cin >> n;
+	kiemtra(n);
+}
