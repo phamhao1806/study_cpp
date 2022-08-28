69eb6e321f9eb375def76e06be1f3b3adf104895
hao <haovippc@gmail.com>
2022-08-28 16:47:44 +0700

    ham mang

--- ham_mang.cpp
+++ ham_mang.cpp
@@ -15,6 +15,22 @@ void hien_thi(int *a, int n){
 	}
 }
 
+void sap_xep(int *a, int n){
+	int tg;
+	printf("\nmang sau khi sap xep tang dan: \n");
+	for(int i = 0; i < n; i++){
+		for(int j = i+1; j < n; j++){
+			if(a[i] > a[j]){
+			    tg = a[i];
+			    a[i] = a[j];
+			    a[j] = tg;
+	     	}
+		}
+	}
+	for(int i = 0; i < n; i++){
+		printf("%4d", a[i]);
+	}
+}
 int main(){
 	int n;
 	printf("nhap so phan tu n: ");
@@ -23,5 +39,6 @@ int main(){
 	a = (int *)malloc(n*sizeof(int));
 	nhap_mang(a, n);
 	hien_thi(a, n);
+	sap_xep(a, n);
 	free(a);
 }
